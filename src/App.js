d3892c550983fa29cba988daba5af4d98410f440
adrian <adrianhajdin00@gmail.com>
2019-01-08 17:54:39 +0100

    remove screens, add conditional rendering, add seperate component for scrollView, add constants, add util functions, refactor app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,130 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import React from 'react';
+import { View, Text } from 'react-native';
 
-import { FirstScreen, SecondScreen, FourthScreen, ThirdScreen } from './Components';
+import styles from './styles/styles';
 
-const RootStack = createStackNavigator({
-  First: { screen: FirstScreen },
-  Second: { screen: SecondScreen },
-  Third: { screen: ThirdScreen },
-  Fourth: { screen: FourthScreen },
-}, {
-  headerMode: 'none',
-});
+import { initialState } from './util/constants';
 
-const App = createAppContainer(RootStack);
+import { InputComponent, InputField, PostaniStudent, Header, ButtonGroup, ScrollView } from './Components';
+
+class App extends React.Component {
+  state = initialState;
+
+  // handleChange = ({ target: { value, name } }) => this.setState({ [name]: value });
+
+  // handleSelectChange = ({ target: { value, name } }) => this.setState({ [name]: value });
+
+  handleBeginning = () => this.setState({ activeStep: 0 })
+
+  handleBack = activeStep => this.setState({ activeStep: activeStep - 1 });
+
+  handlePress = () => {
+    const { activeStep } = this.state;
+
+    if (activeStep === 0) {
+      this.setState({ activeStep: activeStep + 1 });
+    } else if (activeStep === 1) {
+      this.setState({ activeStep: activeStep + 1 });
+    } else if (activeStep === 2) {
+      this.setState({ activeStep: activeStep + 1 });
+    }
+  }
+
+  render() {
+    const { activeStep } = this.state;
+
+    let dialogContent;
+
+    switch (activeStep) {
+      case 0:
+        dialogContent = (
+          <View>
+            <PostaniStudent />
+            <View style={styles.inputContainer}>
+              <InputComponent
+                heading="Ocjene iz srednje škole"
+                paragraph="Ovdje upišite postotak od ukupnog broja bodova koji vam određeni fakultet pridaje za prosjek ocjena srednje škole"
+              />
+              <InputField placeholder="Prosjek svih ocjena*" />
+            </View>
+            <View style={styles.inputContainer}>
+              <InputComponent
+                heading="Obvezni dio državne mature"
+                paragraph="Ovdje upišite postotak od ukupnog broja bodova koji vam određeni fakultet pridaje za obvezni dio državne mature"
+              />
+              <InputField placeholder="Hrvatski jezik*" />
+              <InputField placeholder="Matematika*" />
+              <InputField placeholder="Engleski jezik*" />
+            </View>
+          </View>
+        );
+        break;
+      case 1:
+        dialogContent = (
+          <View>
+            <View style={styles.inputContainer}>
+              <InputComponent
+                heading="Prosjeci ocjena srednje škole"
+                paragraph="Ovdje upišite prosjeke ocjena koje ste postigli tijekom
+        četiri razreda srednje škole"
+              />
+              <InputField placeholder="1. razred*" />
+              <InputField placeholder="2. razred*" />
+              <InputField placeholder="3. razred*" />
+              <InputField placeholder="4. razred*" />
+            </View>
+          </View>
+        );
+        break;
+      case 2:
+        dialogContent = (
+          <View>
+            <View style={styles.inputContainer}>
+              <InputComponent
+                heading="Rezultati mature"
+                paragraph="Ovdje upišite rezultate koje ste postigli na
+        ispitima državne mature"
+              />
+              <InputField placeholder="Hrvatski jezik*" />
+              <InputField placeholder="Matematika*" />
+              <InputField placeholder="Engleski jezik*" />
+            </View>
+          </View>
+        );
+        break;
+      case 3:
+        dialogContent = (
+          <View>
+            <View style={styles.inputContainer}>
+              <Text>Rezultati:</Text>
+              <Text>Ukupan broj bodova:</Text>
+            </View>
+          </View>
+        );
+        break;
+      default:
+        break;
+    }
+
+    return (
+      <ScrollView
+        children={(
+          <React.Fragment>
+            <View>
+              <Header />
+              {dialogContent}
+            </View>
+            <ButtonGroup
+              activeStep={activeStep}
+              onForward={this.handlePress}
+              onBack={() => this.handleBack(activeStep)}
+              onBeginning={this.handleBeginning}
+            />
+          </React.Fragment>
+        )}
+      />
+    );
+  }
+}
 
 export default App;
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,9 +1,6 @@
 export { default as InputComponent } from './InputComponent/InputComponent';
 export { default as InputField } from './InputField/InputField';
 export { default as PostaniStudent } from './PostaniStudent/PostaniStudent';
+export { default as ButtonGroup } from './ButtonGroup/ButtonGroup';
 export { default as Header } from './Header/Header';
-
-export { default as FirstScreen } from './Screens/FirstScreen';
-export { default as SecondScreen } from './Screens/SecondScreen';
-export { default as ThirdScreen } from './Screens/ThirdScreen';
-export { default as FourthScreen } from './Screens/FourthScreen';
+export { default as ScrollView } from './ScrollView/ScrollView';
