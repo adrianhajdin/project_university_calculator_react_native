dc1f9cf9fe0d5706b059769fc6e77bd39d72c64e
adrian <adrianhajdin00@gmail.com>
2019-02-06 21:34:19 +0100

    pre publish setting up

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.kalkulatorbodovazamaturu;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "AwesomeProject";
+        return "KalkulatorBodovaZaMaturu";
     }
 }
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.kalkulatorbodovazamaturu;
 
 import android.app.Application;
 
